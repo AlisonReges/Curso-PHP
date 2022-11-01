d9bd9e993cf5570bf67ca071ce95e3038962151f
Alison Reges <112107547+AlisonReges@users.noreply.github.com>
2022-10-31 21:30:28 -0300

    Update

--- Teste/import.php
+++ Teste/import.php
@@ -4,6 +4,7 @@
     if(isset($_POST['enviar'])){
         $arquivo = $_FILES['file'];
         $arquivoNew = explode('.',$arquivo['name']);
+        var_dump($arquivo['name']);
 
         if($arquivoNew[sizeof($arquivoNew)-1]!='csv'){
             die('Você não pode fazer upload deste tipo de arquivo.');
@@ -13,12 +14,14 @@
             
 
             $sql_code = "
-            LOAD DATA INFILE 'uploads/cadastro.csv'
+            LOAD DATA INFILE 'C:\\\wamp64\\\www\\\Clone\\\Curso-PHP\\\Teste\\\uploads\\\arquivo.csv'
             INTO TABLE cadastros
             FIELDS TERMINATED BY ','
             ENCLOSED BY '\"'
             LINES TERMINATED BY '\\n'
+            IGNORE 1 ROWS
             (nome,niveldeacesso,senha,genero,datadecadastro,cpf);";
+            var_dump($sql_code);
             $query = mysqli_query($link,$sql_code);
             
             if (!$query) {
